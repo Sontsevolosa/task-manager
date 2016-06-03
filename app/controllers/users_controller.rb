42233e6a1ddb19af3d5f4fc5c7eaa2bd40a15e90
sontsevolosa <sontsevolosa@gmail.com>
2016-06-04 01:39:51 +0300

    Adding the user index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index]
 
   def index
+  	@user = User.all
   end
-  
+
   def show
   	@user = User.find(params[:id])
   end
