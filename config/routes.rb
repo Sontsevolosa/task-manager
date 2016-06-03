cc0a0ab2f19dc3c2b92d464f7287d4ab6468d9e1
sontsevolosa <sontsevolosa@gmail.com>
2016-06-03 19:24:15 +0300

    Adding a route for the signup page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root 'tasks#index'
+  get 'signup' => 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
