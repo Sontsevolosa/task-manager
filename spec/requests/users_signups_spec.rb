54c802d4585c5974fbbec21bb735ab9a65cc6a0d
sontsevolosa <sontsevolosa@gmail.com>
2016-06-04 01:20:48 +0300

    Adding the log_out method

--- spec/requests/users_signups_spec.rb
+++ spec/requests/users_signups_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "UsersSignups", type: :request do
   describe "GET /signup_path" do
-    it "request works!" do
+    it "request for user sign up works!" do
       get signup_path
       expect(response).to have_http_status(200)
     end
