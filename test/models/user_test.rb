4829d08398328cf8686d53f8354f5596a66e7da2
sontsevolosa <sontsevolosa@gmail.com>
2016-06-03 20:54:05 +0300

    Testing for a minimum password length

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,4 +9,13 @@ class UserTest < ActiveSupport::TestCase
 	test "should be valid" do
 		assert @user.valid?
 	end
+
+	test "password should be present (nonblank)" do
+		@user.password = @user.password_confirmation = " " * 6
+		assert_not @user.valid?
+	end
+
+	test "password should have a minimum length" do
+		@user.password = @user.password_confirmation = "a" * 5
+	end
 end
