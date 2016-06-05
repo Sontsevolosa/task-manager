89c8b7ca9ec016fa170dc8742020414302639b2c
sontsevolosa <sontsevolosa@gmail.com>
2016-06-05 20:36:15 +0300

    Adding destroy action to the Tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
 	before_action :logged_in_user, only: [:create, :destroy]
-
+	before_action :correct_user, only: :destroy
+	
 	def index
 		@tasks = Task.all.paginate(page: params[:page])
 		@task = Task.new
@@ -17,9 +18,20 @@ class TasksController < ApplicationController
 		end
 	end
 
+	def destroy
+		@task.destroy
+		flash[:success] = "Task deleted"
+		redirect_to request.referrer || root_url
+	end
+
 	private
 
 		def task_params
 			params.require(:task).permit(:content)
+		end
+
+		def correct_user
+			@task = current_user.tasks.find_by(id: params[:id])
+			redirect_to root_url if @task.nil?
 		end
 end
