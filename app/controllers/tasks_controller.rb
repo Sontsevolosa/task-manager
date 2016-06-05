31dc2f4c0b7142c33847eda60b9ede48f50e12c9
sontsevolosa <sontsevolosa@gmail.com>
2016-06-05 19:23:17 +0300

    Adding tasks creation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,10 +3,12 @@ class TasksController < ApplicationController
 
 	def index
 		@tasks = Task.all.paginate(page: params[:page])
+		@task = Task.new
 	end
 
 	def create
 		@task = current_user.tasks.build(task_params)
+
 		if @task.save
 			flash[:success] = "Task created!"
 			redirect_to root_url
