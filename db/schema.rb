d303754db26cafdf775639c9e0c9cc42c1934a19
sontsevolosa <sontsevolosa@gmail.com>
2016-06-05 14:38:15 +0300

    Adding Task model validation for content

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603174033) do
+ActiveRecord::Schema.define(version: 20160605112315) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
