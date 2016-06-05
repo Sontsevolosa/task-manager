aa78bcb4628c36da79572688f980a252c02e1b72
sontsevolosa <sontsevolosa@gmail.com>
2016-06-05 20:16:17 +0300

    Fixing tests errors

--- spec/requests/tasks_showings_spec.rb
+++ spec/requests/tasks_showings_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "TasksShowings", type: :request do
-  describe "GET /tasks_path" do
+  describe "GET /index" do
     it "request for tasks index page works!" do
-      get tasks_path
+      get root_path
       expect(response).to have_http_status(200)
     end
   end
