f760848bdae5c73d886eaa985d9b28fafa7eefa9
sontsevolosa <sontsevolosa@gmail.com>
2016-06-05 15:56:58 +0300

    Adding routes for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy'
   resources :users
+  resources :tasks
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
