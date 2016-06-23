7428ef9888c54d2c78bee3817003d45a7a1f01e3
sontsevolosa <sontsevolosa@gmail.com>
2016-06-23 16:27:02 +0300

    Changing spaces

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,31 +2,31 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index]
 
   def index
-  	@users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
   end
 
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   def create
-  	@user = User.new(user_params)
-  	if @user.save
-  		log_in @user
-  		flash[:success] = "Welcome to the task manager!"
-  		redirect_to root_url
-  	else
-  		render 'new'
-  	end
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the task manager!"
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
   private
 
-  	def user_params
-  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  	end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,33 +1,33 @@
 module SessionsHelper
 
-	# Logs in the given user
-	def log_in(user)
-		session[:user_id] = user.id
-	end
+  # Logs in the given user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 
-	# Returns true if the given user is the current user
-	def current_user?(user)
-		user == current_user
-	end
+  # Returns true if the given user is the current user
+  def current_user?(user)
+    user == current_user
+  end
 
-	# Returns the current logged-in user
-	def current_user
-		@current_user ||= User.find_by(id: session[:user_id])
-	end
+  # Returns the current logged-in user
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 
-	# Returns true if the user is logged in, false otherwise
-	def logged_in?
-		!current_user.nil?
-	end
+  # Returns true if the user is logged in, false otherwise
+  def logged_in?
+    !current_user.nil?
+  end
 
-	# Logs out the current user
-	def log_out
-		session.delete(:user_id)
-		@current_user = nil
-	end
+  # Logs out the current user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 
-	# Stores the URL trying to be accessed
-	def store_location
-		session[:forwarding_url] = request.url if request.get?
-	end
+  # Stores the URL trying to be accessed
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 end
